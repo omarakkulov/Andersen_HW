7fd0754371a31ae2ae9c9b8cd5337df703c92011
Omar Akkulov <omar-akkulov@mail.ru>
2021-05-05 18:29:16 +0300

    task3

--- src/main/java/ru/akkulov/servlets/HelloServlet.java
+++ src/main/java/ru/akkulov/servlets/HelloServlet.java
@@ -1,19 +1,25 @@
 package ru.akkulov.servlets;
 
+import ru.akkulov.model.Employee;
+import ru.akkulov.service.EmployeeService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 public class HelloServlet extends HttpServlet {
+    private static final String index = "/view/helloJSP.jsp";
+    List<Employee> employeeList;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw = resp.getWriter();
+        EmployeeService service = new EmployeeService();
+        employeeList = service.getAll();
 
-        pw.println("<html>");
-        pw.println("<h1> Hello world! <h1>");
-        pw.println("<html>");
+        req.setAttribute("employee", employeeList);
+        req.getRequestDispatcher(index).forward(req, resp);
     }
 }
